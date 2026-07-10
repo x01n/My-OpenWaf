c38e3fe687d3ea3ff6169d013d463c8416ebe8f1
chanhanzhan <chanhanzhan@dailianshop.cn>
2026-07-10 20:45:17 +0800

    feat: 增加自定义的第三方库

---
+++ third_party/hertz-contrib-http2/config/option.go
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
+type Config struct {
+	DisableKeepalive bool
+	EnableTrace      bool
+	ReadTimeout      time.Duration
+
+	// MaxHeaderListSize optionally specifies the maximum header list size
+	// the server is willing to accept. If zero, a default value is used.
+	MaxHeaderListSize uint32
+
+	// MaxHeaderFields optionally specifies the maximum number of header
+	// fields the server is willing to accept. If zero, no explicit limit
+	// is enforced on the field count.
+	MaxHeaderFields int
+
+	// MaxHandlers limits the number of http.Handler ServeHTTP goroutines
+	// which may run at a time over all connections.
+	// Negative or zero no limit.
+	// TODO: implement
+	MaxHandlers int
+
+	// MaxConcurrentStreams optionally specifies the number of
+	// concurrent streams that each client may have open at a
+	// time. This is unrelated to the number of http.Handler goroutines
+	// which may be active globally, which is MaxHandlers.
+	// If zero, MaxConcurrentStreams defaults to at least 100, per
+	// the HTTP/2 spec's recommendations.
+	MaxConcurrentStreams uint32
+
+	// MaxReadFrameSize optionally specifies the largest frame
+	// this server is willing to read. A valid value is between
+	// 16k and 16M, inclusive. If zero or otherwise invalid, a
+	// default value is used.
+	MaxReadFrameSize uint32
+
+	// PermitProhibitedCipherSuites, if true, permits the use of
+	// cipher suites prohibited by the HTTP/2 spec.
+	PermitProhibitedCipherSuites bool
+
+	// IdleTimeout specifies how long until idle clients should be
+	// closed with a GOAWAY frame. PING frames are not considered
+	// activity for the purposes of IdleTimeout.
+	IdleTimeout time.Duration
+
+	// MaxUploadBufferPerConnection is the size of the initial flow
+	// control window for each connections. The HTTP/2 spec does not
+	// allow this to be smaller than 65535 or larger than 2^32-1.
+	// If the value is outside this range, a default value will be
+	// used instead.
+	MaxUploadBufferPerConnection int32
+
+	// MaxUploadBufferPerStream is the size of the initial flow control
+	// window for each stream. The HTTP/2 spec does not allow this to
+	// be larger than 2^32-1. If the value is zero or larger than the
+	// maximum, a default value will be used instead.
+	MaxUploadBufferPerStream int32
+}
+
+// Option is the only struct that can be used to set HTTP2 Config.
+type Option struct {
+	F func(o *Config)
+}
+
+func (o *Config) Apply(opts []Option) {
+	for _, op := range opts {
+		op.F(o)
+	}
+}
+
+// WithReadTimeout is used to set the read timeout.
+func WithReadTimeout(t time.Duration) Option {
+	return Option{F: func(o *Config) {
+		o.ReadTimeout = t
+	}}
+}
+
+// WithDisableKeepAlive is used to set whether disableKeepAlive.
+func WithDisableKeepAlive(disableKeepAlive bool) Option {
+	return Option{F: func(o *Config) {
+		o.DisableKeepalive = disableKeepAlive
+	}}
+}
+
+// WithMaxConcurrentStreams is used to set the max concurrent streams.
+func WithMaxConcurrentStreams(n uint32) Option {
+	return Option{F: func(o *Config) {
+		o.MaxConcurrentStreams = n
+	}}
+}
+
+// WithMaxReadFrameSize is used to set the max read frame size.
+func WithMaxReadFrameSize(n uint32) Option {
+	return Option{F: func(o *Config) {
+		o.MaxReadFrameSize = n
+	}}
+}
+
+// WithPermitProhibitedCipherSuites is used to set whether permit prohibited chipher suites.
+func WithPermitProhibitedCipherSuites(permitProhibitedChipherSuites bool) Option {
+	return Option{F: func(o *Config) {
+		o.PermitProhibitedCipherSuites = permitProhibitedChipherSuites
+	}}
+}
+
+// WithIdleTimeout is used to set idle timeout.
+func WithIdleTimeout(t time.Duration) Option {
+	return Option{F: func(o *Config) {
+		o.IdleTimeout = t
+	}}
+}
+
+// WithMaxUploadBufferPerConnection is used to set max upload buffer per connection.
+func WithMaxUploadBufferPerConnection(n int32) Option {
+	return Option{F: func(o *Config) {
+		o.MaxUploadBufferPerConnection = n
+	}}
+}
+
+// WithMaxUploadBufferPerStream is used to set max upload buffer per stream.
+func WithMaxUploadBufferPerStream(n int32) Option {
+	return Option{F: func(o *Config) {
+		o.MaxUploadBufferPerStream = n
+	}}
+}
+
+func NewConfig(opts ...Option) *Config {
+	c := &Config{
+		IdleTimeout: consts.DefaultMaxIdleConnDuration,
+	}
+	c.Apply(opts)
+	return c
+}
+
+// WithServerMaxHeaderListSize is used to set the max header list size.
+func WithServerMaxHeaderListSize(n uint32) Option {
+	return Option{F: func(o *Config) {
+		o.MaxHeaderListSize = n
+	}}
+}
+
+// WithServerMaxHeaderFields is used to set the max header fields count.
+func WithServerMaxHeaderFields(n int) Option {
+	return Option{F: func(o *Config) {
+		o.MaxHeaderFields = n
+	}}
+}
---
+++ third_party/hertz-contrib-http2/stream.go
@@ -0,0 +1,100 @@
+/*
+ *	Copyright 2022 CloudWeGo Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2014 The Go Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package http2
+
+import (
+	"context"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/hertz-contrib/http2/internal/bytesconv"
+)
+
+// stream represents a stream. This is the minimal metadata needed by
+// the serve goroutine. Most of the actual stream state is owned by
+// the http.Handler's goroutine in the responseWriter. Because the
+// responseWriter's responseWriterState is recycled at the end of a
+// handler, this struct intentionally has no pointer to the
+// *responseWriter{,State} itself, as the Handler ending nils out the
+// responseWriter's state field.
+type stream struct {
+	// immutable:
+	sc      *serverConn
+	id      uint32
+	body    *pipe       // non-nil if expecting DATA frames
+	cw      closeWaiter // closed wait stream transitions to closed state
+	reqCtx  *app.RequestContext
+	baseCtx context.Context
+	cancel  context.CancelFunc // 在 RST_STREAM 时取消 handler 上下文
+
+	// owned by serverConn's serve loop:
+	bodyBytes        int64 // body bytes seen so far
+	declBodyBytes    int64 // or -1 if undeclared
+	flow             flow  // limits writing from Handler to client
+	inflow           flow  // what the client is allowed to POST/etc to us
+	state            streamState
+	resetQueued      bool        // RST_STREAM queued for write; set by sc.resetStream
+	gotTrailerHeader bool        // HEADER frame for trailers was seen
+	wroteHeaders     bool        // whether we wrote headers (not status 100)
+	writeDeadline    *time.Timer // nil if unused
+	rw               *responseWriter
+	handler          app.HandlerFunc
+
+	trailer []trailerKV
+}
+
+type trailerKV struct {
+	key   string
+	value string
+}
+
+func (st *stream) processTrailerHeaders(f *MetaHeadersFrame) error {
+	sc := st.sc
+	sc.serveG.check()
+	if st.gotTrailerHeader {
+		return streamError(st.id, ErrCodeProtocol)
+	}
+	st.gotTrailerHeader = true
+	if !f.StreamEnded() {
+		return streamError(st.id, ErrCodeProtocol)
+	}
+
+	if len(f.PseudoFields()) > 0 {
+		return streamError(st.id, ErrCodeProtocol)
+	}
+
+	if st.trailer == nil {
+		st.trailer = make([]trailerKV, 0, len(f.RegularFields()))
+	}
+	for _, hf := range f.RegularFields() {
+		key := st.sc.canonicalHeader(hf.Name)
+		st.trailer = append(st.trailer, trailerKV{key, hf.Value})
+	}
+
+	st.endStream()
+	return nil
+}
+
+func (st *stream) copyTrailer() {
+	for _, kv := range st.trailer {
+		st.reqCtx.Request.Header.Trailer().UpdateArgBytes(bytesconv.S2b(kv.key), bytesconv.S2b(kv.value))
+	}
+}
